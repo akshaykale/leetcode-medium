b974348dea399d350181e5ae74fcaae398bd8f1f
Akshay Kale <ark.akshaykale@gmail.com>
2021-05-19 16:34:03 +0900

    437. Path Sum III

---
+++ 437. Path Sum III.kt
@@ -0,0 +1,69 @@
+/**
+https://leetcode.com/problems/path-sum-iii/
+
+Given the root of a binary tree and an integer targetSum, return the number of paths where the sum of the values along the path equals targetSum.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (i.e., traveling only from parent nodes to child nodes).
+
+ 
+
+Example 1:
+
+
+Input: root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+Output: 3
+Explanation: The paths that sum to 8 are shown.
+Example 2:
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: 3
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 1000].
+-109 <= Node.val <= 109
+-1000 <= targetSum <= 1000
+*/
+
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    
+    var count = 0
+    val list = mutableListOf<Int>() 
+    
+    fun pathSum(root: TreeNode?, targetSum: Int): Int {
+        dfs(root, targetSum)
+        return count
+    }
+    
+    fun dfs(root: TreeNode?, targetSum: Int) {
+        
+        if(root == null) return
+        
+        list.add(root.`val`)
+        
+        dfs(root.left, targetSum)
+        dfs(root.right, targetSum)
+        
+        var sum = targetSum
+        for (i in list.size-1 downTo 0 step 1) {
+            sum -= list.get(i)
+            if (sum == 0) {
+                count ++
+            }
+        }
+        
+        list.removeAt(list.size-1)
+    }
+}
